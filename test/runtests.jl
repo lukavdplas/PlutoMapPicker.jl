39c472f91c6fd7949fc15c85205f21cb67cb3e84
Luka van der Plas <lukavdplas@mailfence.com>
2024-03-20 21:06:14 +0100

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,23 @@ using HypertextLiteral
 using Test
 
 @testset "PlutoMapPicker.jl" begin
-    @test typeof(PlutoMapPicker.MapPicker(0.0, 0.0, 1)) == HypertextLiteral.Result
-    @test typeof(PlutoMapPicker.MapPickerMultiple(0.0, 0.0, 1)) == HypertextLiteral.Result
-    @test typeof(PlutoMapPicker.osm_tile_layer) == PlutoMapPicker.TileLayer
+    @testset "tile layers" begin
+        @test typeof(PlutoMapPicker.osm_tile_layer) == PlutoMapPicker.TileLayer
+        @test typeof(PlutoMapPicker.osm_tile_layers.standard) == PlutoMapPicker.TileLayer
+        @test typeof(PlutoMapPicker.stadia_tile_layers.osm_bright) == PlutoMapPicker.TileLayer
+    end
+
+    @testset "MapPicker" begin
+        @test typeof(PlutoMapPicker.MapPicker(0.0, 0.0, 1)) == HypertextLiteral.Result
+
+        alt_tile_layer = PlutoMapPicker.osm_tile_layers.humanitarian
+        @test typeof(PlutoMapPicker.MapPicker(0.0, 0.0, 1; tile_layer = alt_tile_layer)) == HypertextLiteral.Result
+    end
+
+    @testset "MapPickerMultiple" begin
+        @test typeof(PlutoMapPicker.MapPickerMultiple(0.0, 0.0, 1)) == HypertextLiteral.Result
+
+        alt_tile_layer = PlutoMapPicker.osm_tile_layers.humanitarian
+        @test typeof(PlutoMapPicker.MapPickerMultiple(0.0, 0.0, 1; tile_layer = alt_tile_layer)) == HypertextLiteral.Result
+    end
 end
